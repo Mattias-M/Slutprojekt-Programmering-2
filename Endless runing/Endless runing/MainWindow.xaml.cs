1c0e99e726fdfb137da7f452cb409857b411c363
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2021-04-29 09:55:24 +0200

    endless
    jag är en kvart inne i videon . har gjort klar allt inne i xaml och har klar med några metoder.

--- Endless runing/Endless runing/MainWindow.xaml.cs
+++ Endless runing/Endless runing/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace Endless_runing
 {
@@ -19,10 +21,100 @@ namespace Endless_runing
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
+
     {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        Rect playerHitBox;
+        Rect groundHitBox;
+
+        bool jumping;
+
+        int force = 20;
+        int speed = 5;
+
+        Random rnd = new Random();
+
+        bool gameOver;
+
+        double spriteIndex = 0;
+
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgrundSprite = new ImageBrush();
+        ImageBrush obstacleSprite = new ImageBrush();
+
+        int[] obstacalePostive = { 320 ,310 ,300, 315};
+
+        int score = 0;
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MyCanvas.Focus();
+
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            
+            backgrundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,image/background.gif"));
+
+            background.Fill = backgrundSprite;
+            background2.Fill = backgrundSprite;
+
+            StartGame();
+
+
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+         private void StartGame()
+        {
+            Canvas.SetLeft(background, 0);
+            Canvas.SetLeft(background2, 140);
+
+            Canvas.SetLeft(player, 110);
+            Canvas.SetTop(player, 310);
+
+            RunSprits(1);
+
+            obstacleSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,image/obstacle.gif"));
+            obstacle.Fill = obstacleSprite;
+
+             jumping = false;
+             gameOver = false;
+
+             score = 0;
+
+
+
+
+
+        }
+
+        private void RunSprits(double i)
+        {
+
         }
     }
 }
